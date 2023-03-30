d09a51eaf82927f094b46ab7722a9eefdb0b211e
Marco Gervatini <marcogerva07@gmail.com>
2023-03-30 16:16:05 +0200

    aggiunto pattern strategy common cards.

---
+++ src/Model/Cards/CommonCard_5.java
@@ -0,0 +1,33 @@
+package Model.Cards;
+
+public class CommonCard_5 extends CommonCard implements CommonCardStrategy {
+    //serve un check che i punti token del giocatore siano =0 per fare i controlli.
+    private int current_points = 8;
+
+    public int Mission_1(){
+        /**/
+        return 1;
+    }
+
+    @Override
+    public int isCompleted(){
+        return Mission_1();
+        // se la condizione e' soddisfatta chiama riscale
+    }
+    /*
+    Le carte possono essere classificare in 5 macro categorie in modo da creare algoritmi diversi per il
+    conteggio dei punti, le categoria sono così divise:
+    1. Numero di adiacenze ripetute n volte dello stesso tipo di tessere (n. 1, 3)
+    2. Posizione relativa fissa ripetute n volte dello stesso tipo di tessere (n. 2, 4, ,7, 9, 10, 11)
+    3. Posizione relativa fissa ripetute n volte anche di tipo tiverso di tessere (n. 5, 8)
+    4. Tessera singola n. 6
+    5. Tessera singola n. 12
+     */
+    /*
+    Per la categoria 4 e 5 si nel detta gli la struttura richiesta
+    Per la categoria 1 si cercano il numero di adiacenze e le sue ripetute tenendo fisso il tipo di tessera
+    Per la categoria 2 si cerca la struttura relativa e le sue ripetute tenendo fisso il tipo di tessera
+    Per la categoria 3 si cercano la struttura relativa e le sue ripetute tenendo variabile il nunero di tessera
+     */
+    //probabilmente la categoria 2,3 possono essere unite
+}
