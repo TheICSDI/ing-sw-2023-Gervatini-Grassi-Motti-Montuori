8ca94c2e6b5eb5040a6b9d83ec6248ea4da0c770
Andrea Grassi <andrea10.grassi@mail.polimi.it>
2023-04-03 13:56:32 +0200

    Pick TIles

--- src/Model/Position.java
+++ src/Model/Position.java
@@ -2,6 +2,18 @@
 package Model;
 
 public class Position {
-    public int x;
-    public int y;
+    private int x;
+    private int y;
+    public Position(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
