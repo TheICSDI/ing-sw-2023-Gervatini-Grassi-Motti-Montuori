5a8a888c5da4e70015be7efd9d18b970f2cb9ca3
Andrea Grassi <andrea10.grassi@mail.polimi.it>
2023-04-03 15:54:36 +0200

    Pick TIles

--- src/Model/Cards/CC_02.java
+++ src/Model/Cards/CC_02.java
@@ -1,12 +1,37 @@
 package Model.Cards;
 
 import Model.Player;
+import Model.Tile.type;
 
 public class CC_02 implements CCStrategy {
     public boolean isCompleted(Player p) {
         /*
         inserire alg
          */
+        /*Tile[][] current_shelf = p.getShelf();
+        type color;
+        boolean end=true;
+        /*
+        check if any of the four tile is empty.
+
+        if (current_shelf[0][0].getCategory() == type.EMPTY ||
+                current_shelf[0][5].getCategory() == type.EMPTY ||
+                current_shelf[4][0].getCategory() == type.EMPTY ||
+                current_shelf[4][5].getCategory() == type.EMPTY){
+            end=false;
+        }
+        else{
+            /*
+            check if all the tiles have the same type of the most low-left one.
+
+            color = current_shelf[0][0].getCategory();
+            if(color!= current_shelf[0][5].getCategory() ||
+                    color != current_shelf[4][0].getCategory() ||
+                    color != current_shelf[4][5].getCategory()){
+                end = false;
+            }
+        }
+        return end;*/
         return true;
     }
     /*
--- src/Model/Cards/CC_03.java
+++ src/Model/Cards/CC_03.java
@@ -5,29 +5,43 @@ import Model.Tile.type;
 
 public class CC_03 implements CCStrategy {
     public boolean isCompleted(Player p) {
-        Tile[][] current_shelf = p.getShelf();
-        type color;
-        boolean end=true;
-        /*
-        check if any of the four tile is empty.
-         */
-        if (current_shelf[0][0].getCategory() == type.EMPTY ||
-                current_shelf[0][5].getCategory() == type.EMPTY ||
-                current_shelf[4][0].getCategory() == type.EMPTY ||
-                current_shelf[4][5].getCategory() == type.EMPTY){
-            end=false;
-        }
-        else{
-            /*
-            check if all the tiles have the same type of the most low-left one.
-             */
-            color = current_shelf[0][0].getCategory();
-            if(color!= current_shelf[0][5].getCategory() ||
-                color != current_shelf[4][0].getCategory() ||
-                color != current_shelf[4][5].getCategory()){
-                end = false;
+        Tile[][] curr_shelf=p.getShelf();
+        boolean[][] Seen=new boolean[5][6];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                Seen[i][j]=false;
             }
+
         }
-        return end;
+        int adj=0,limit=0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+               type curr=curr_shelf[i][j].getCategory();
+               try{
+                   if(!Seen[i][j]) {
+                       Seen[i][j]=true;
+                       adj = adj(curr, i, j, curr_shelf, Seen);
+                   }
+               }catch (IndexOutOfBoundsException ignored){}
+               if(adj>=4){
+                   limit++;
+               }
+            }
+
+        }
+        return limit >= 4;
+    }
+
+    private int adj(type c,int x,int y, Tile[][] shelf,boolean[][] Seen) {
+        int p = 1;
+        if (shelf[x+1][y].getCategory() == c && !Seen[x+1][y]) {
+            Seen[x+1][y]=true;
+            p = p + adj(c, x+1, y, shelf, Seen);
+        }
+        if (shelf[x][y+1].getCategory() == c && !Seen[x][y+1]) {
+                Seen[x][y+1]=true;
+                p = p + adj(c, x, y+1, shelf, Seen);
+        }
+        return p;
     }
 }
