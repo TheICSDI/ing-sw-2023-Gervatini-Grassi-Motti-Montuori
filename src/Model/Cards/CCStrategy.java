f5ce061b5df4ac1ef07c8a030724715deb227cb6
Marco Gervatini <marcogerva07@gmail.com>
2023-04-04 13:36:09 +0200

    implemented CC strategies no 3,6,7,9,12, with comments

--- src/Model/Cards/CCStrategy.java
+++
@@ -1,8 +0,0 @@
-package Model.Cards;
-
-import Model.Player;
-
-public interface CCStrategy {
-    boolean isCompleted(Player p);
-
-}
--- src/Model/Position.java
+++
@@ -1,7 +0,0 @@
-/** Represents a pair of indices used to indicate a position on the board */
-package Model;
-
-public class Position {
-    public int x;
-    public int y;
-}
