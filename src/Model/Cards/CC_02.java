a19803d21a9b70276748959fa4cbf9ddd877bb60
TheICSDI <giulio2002montuori@gmail.com>
2023-04-04 14:02:05 +0200

    fix try catch in CC_03 and removed commented code in CC_02

--- src/Model/Cards/CC_02.java
+++ src/Model/Cards/CC_02.java
@@ -2,52 +2,41 @@ package Model.Cards;
 
 import Model.Player;
 import Model.Tile.type;
+import Model.Tile.Tile;
 
+/**
+ * CC_02 class implements the logic for checking if the first common goal card is completed by a player.
+ * The first common goal card requires four tiles of the same type in the four corners of the bookshelf.
+ */
 public class CC_02 implements CCStrategy {
-    public boolean isCompleted(Player p) {
-        /*
-        inserire alg
-         */
-        /*Tile[][] current_shelf = p.getShelf();
-        type color;
-        boolean end=true;
-        /*
-        check if any of the four tile is empty.
+	/**
+	 * Checks if the common goal is completed
+	 * @param p Player
+	 * @return a boolean if the common goal card is completed
+	 */
+	public boolean isCompleted(Player p) {
+	Tile[][] current_shelf = p.getShelf();
+	type color;
+	boolean end=true;
+	
+	// check if any of the four tile is empty.
 
-        if (current_shelf[0][0].getCategory() == type.EMPTY ||
-                current_shelf[0][5].getCategory() == type.EMPTY ||
-                current_shelf[4][0].getCategory() == type.EMPTY ||
-                current_shelf[4][5].getCategory() == type.EMPTY){
-            end=false;
-        }
-        else{
-            /*
-            check if all the tiles have the same type of the most low-left one.
+	if (current_shelf[0][0].getCategory() == type.EMPTY ||
+			current_shelf[0][5].getCategory() == type.EMPTY ||
+			current_shelf[4][0].getCategory() == type.EMPTY ||
+			current_shelf[4][5].getCategory() == type.EMPTY){
+		end=false;
+	}
+	else{
+		// check if all the tiles have the same type of the most low-left one.
 
-            color = current_shelf[0][0].getCategory();
-            if(color!= current_shelf[0][5].getCategory() ||
-                    color != current_shelf[4][0].getCategory() ||
-                    color != current_shelf[4][5].getCategory()){
-                end = false;
-            }
-        }
-        return end;*/
-        return true;
+		color = current_shelf[0][0].getCategory();
+		if(color!= current_shelf[0][5].getCategory() ||
+				color != current_shelf[4][0].getCategory() ||
+				color != current_shelf[4][5].getCategory()){
+			end = false;
+		}
+	}
+	return end;
     }
-    /*
-    Le carte possono essere classificare in 5 macro categorie in modo da creare algoritmi diversi per il
-    conteggio dei punti, le categoria sono così divise:
-    1. Numero di adiacenze ripetute n volte dello stesso tipo di tessere (n. 1, 3)
-    2. Posizione relativa fissa ripetute n volte dello stesso tipo di tessere (n. 2, 4, ,7, 9, 10, 11)
-    3. Posizione relativa fissa ripetute n volte anche di tipo tiverso di tessere (n. 5, 8)
-    4. Tessera singola n. 6
-    5. Tessera singola n. 12
-     */
-    /*
-    Per la categoria 4 e 5 si nel detta gli la struttura richiesta
-    Per la categoria 1 si cercano il numero di adiacenze e le sue ripetute tenendo fisso il tipo di tessera
-    Per la categoria 2 si cerca la struttura relativa e le sue ripetute tenendo fisso il tipo di tessera
-    Per la categoria 3 si cercano la struttura relativa e le sue ripetute tenendo variabile il nunero di tessera
-     */
-    //probabilmente la categoria 2,3 possono essere unite
 }
--- src/Model/Cards/CC_03.java
+++ src/Model/Cards/CC_03.java
@@ -6,23 +6,23 @@ import Model.Tile.type;
 public class CC_03 implements CCStrategy {
     public boolean isCompleted(Player p) {
         Tile[][] curr_shelf=p.getShelf();
-        boolean[][] Seen=new boolean[5][6];
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 6; j++) {
+		int num_row = curr_shelf.length;
+		int num_col = curr_shelf[0].length;
+        boolean[][] Seen=new boolean[num_row][num_col];
+        for (int i = 0; i < num_row; i++) {
+            for (int j = 0; j < num_col; j++) {
                 Seen[i][j]=false;
             }
 
         }
         int adj=0,limit=0;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 6; j++) {
+        for (int i = 0; i < num_row; i++) {
+            for (int j = 0; j < num_col; j++) {
                type curr=curr_shelf[i][j].getCategory();
-               try{
-                   if(!Seen[i][j]) {
-                       Seen[i][j]=true;
-                       adj = adj(curr, i, j, curr_shelf, Seen);
-                   }
-               }catch (IndexOutOfBoundsException ignored){}
+			   if(!Seen[i][j]) {
+				   Seen[i][j]=true;
+				   adj = adj(curr, i, j, curr_shelf, Seen);
+			   }
                if(adj>=4){
                    limit++;
                }
@@ -34,14 +34,18 @@ public class CC_03 implements CCStrategy {
 
     private int adj(type c,int x,int y, Tile[][] shelf,boolean[][] Seen) {
         int p = 1;
-        if (shelf[x+1][y].getCategory() == c && !Seen[x+1][y]) {
-            Seen[x+1][y]=true;
-            p = p + adj(c, x+1, y, shelf, Seen);
-        }
-        if (shelf[x][y+1].getCategory() == c && !Seen[x][y+1]) {
-                Seen[x][y+1]=true;
-                p = p + adj(c, x, y+1, shelf, Seen);
-        }
+		try{
+			if (shelf[x+1][y].getCategory() == c && !Seen[x+1][y]) {
+				Seen[x+1][y]=true;
+				p = p + adj(c, x+1, y, shelf, Seen);
+			}
+		}catch (IndexOutOfBoundsException ignored){}
+		try{
+			if (shelf[x][y+1].getCategory() == c && !Seen[x][y+1]) {
+					Seen[x][y+1]=true;
+					p = p + adj(c, x, y+1, shelf, Seen);
+			}
+		}catch (IndexOutOfBoundsException ignored){}
         return p;
     }
 }
