8e8c073467708115efac6a7f2be9fa637f58694e
TheICSDI <giulio2002montuori@gmail.com>
2023-06-10 19:18:57 +0200

    fix a bug with CC01 and add a couple of new tests of shelfs

--- src/main/java/it/polimi/ingsw/model/Cards/CC_01.java
+++ src/main/java/it/polimi/ingsw/model/Cards/CC_01.java
@@ -26,21 +26,33 @@ public class CC_01 implements CCStrategy {
         int count = 0;
         int num_row = current_shelf.length;
         int num_col = current_shelf[0].length;
+        int new_j, new_i;
         type current_tile;
 
         for(int i = 0; i < num_row; i++)
         {
             for(int j = 0; j < num_col; j++)
             {
+                new_i = 0;
+                new_j = 0;
                 current_tile = current_shelf[i][j].getCategory();
                 if(!current_tile.equals(type.EMPTY)) {
+
                     if (!(
-                            // Check if the current tile is NOT part of a group in the previous row or column
-                            (j != 0 && current_shelf[i][j - 1].getCategory().equals(current_tile))
-                                    || (i != 0 && current_shelf[i - 1][j].getCategory().equals(current_tile)))
-                            // Check if the current tile is part of a group in the next row or column
-                            && ((j + 1 < num_col && current_shelf[i][j + 1].getCategory().equals(current_tile))
-                            || (i + 1 < num_row && current_shelf[i + 1][j].getCategory().equals(current_tile)))) {
+                            // The current tile is not the same as the tile to the left
+                            (j != 0 && current_shelf[i][j - 1].getCategory() == current_tile)
+                                    // The current tile is not the same as the tile above
+                                    || (i != 0 && current_shelf[i - 1][j].getCategory() == current_tile)
+                                    // The next tile to the right is not the same as the tile to its right
+                                    || (j + 2 < num_col && current_shelf[i][j + 1].getCategory() == current_tile && current_shelf[i][j + 2].getCategory() == current_tile)
+                                    // The next tile down is not the same as the tile below it
+                                    || (i + 2 < num_row && current_shelf[i + 1][j].getCategory() == current_tile && current_shelf[i + 2][j].getCategory() == current_tile))
+                            && (
+                            // The current tile is the same as the tile to the right
+                            (j + 1 < num_col && current_shelf[i][j + 1].getCategory() == current_tile)
+                                    // The current tile is the same as the tile below
+                                    || (i + 1 < num_row && current_shelf[i + 1][j].getCategory() == current_tile))) {
+
                         count++;
                         if (count == 6) {
                             return true;
